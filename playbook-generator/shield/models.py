2db73076812165c1a72d48d5e42dec805e0e1e7d
haresudhan <code@0x6c.dev>
2020-08-22 00:29:44 -0600

    Shield fix

--- playbook-generator/shield/models.py
+++ playbook-generator/shield/models.py
@@ -189,7 +189,7 @@ class ShieldElement:
     def to_markdown(self) -> str:
         procedures_md = "#### Procedures\n" + \
             "\n".join([x.to_markdown() for x in self.procedures])
-        return "\n".join(["# Shield Active Defense", self.technique.to_markdown(),
+        return "\n".join(["## Shield Active Defense", self.technique.to_markdown(),
                           self.opportunity.to_markdown(),
                           self.use_case.to_markdown(), procedures_md
                           ])
